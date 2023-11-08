b25c10da55cbf00e7b462ed0e40186852ca0b4e8
atucci <alessiotucci33@gmail.com>
2023-11-08 10:08:02 +0100

    started working on minishell core structure

--- minishell.c
+++ minishell.c
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 09:10:19 by atucci            #+#    #+#             */
-/*   Updated: 2023/11/08 10:06:40 by atucci           ###   ########.fr       */
+/*   Created: 2023/11/08 10:07:19 by atucci            #+#    #+#             */
+/*   Updated: 2023/11/08 10:07:21 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include <stdio.h>
-# include "libft/libft.h"
-# include "builtin_folder/built_ins.h"
-# include "lexical_analysis_folder/lexical_analysis.h"
-# include "recursive_parsing_tree/recursive_parsing_tree.h"
-# include "tokenizer_folder/tokenizer.h"
 
-void	token();
-void	parser();
-void	lexer();
-void	try_builtin();
