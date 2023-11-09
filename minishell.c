19c28c4cb29ac496f5bcd728767d24317986d10a
atucci <alessiotucci33@gmail.com>
2023-11-09 10:02:57 +0100

    ok now we work

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:07:19 by atucci            #+#    #+#             */
-/*   Updated: 2023/11/08 13:40:48 by atucci           ###   ########.fr       */
+/*   Updated: 2023/11/09 09:05:21 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 int	main(int ac, char *av[], char *envp[])
 {
-	ft_printf("checking if makefile works\n");
+	int	count;
+
+	count = 1;
 	if (ac || av || envp)
-	ft_printf("ok");
+	while (av[count])
+	{
+		ft_printf("argoument [%d], %s\t\t", count, av[count]);
+		count++;
+	}
 	lexer();
-//	token();
+	token();
 	parser();
 	try_builtin();
 	return (0);
