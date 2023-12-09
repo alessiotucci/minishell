c6833b5fb254a0b04110aae1d1b3690450a25989
atucci <alessiotucci33@gmail.com>
2023-12-09 17:17:15 +0100

    adding small detail of pork-aroung

--- lexical_analysis_folder/lexical_analysis.c
+++ lexical_analysis_folder/lexical_analysis.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:14:09 by atucci            #+#    #+#             */
-/*   Updated: 2023/12/09 17:04:17 by atucci           ###   ########.fr       */
+/*   Updated: 2023/12/09 17:16:30 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,20 @@ int	lexer(char *string)
 	char *new_string;
 
 	token_head = NULL;
-	// scan for " " or ' ' or ( ) 
+	// scan for " " or ' ' 
 	new_string = replace_me(string, '"', '\t', ' ');
+	new_string = replace_me(string, 39, '\t', ' ');
 	line_of_commands = ft_split(string, ' ');
 	while (line_of_commands[i])
 	{
 	line_of_commands[i] = replace_me(line_of_commands[i], '"', ' ', '\t');
+	line_of_commands[i] = replace_me(line_of_commands[i], 39, ' ', '\t');
 	if (my_strcmp(line_of_commands[i], "exit") == 0)
 		exit(0);
 	create_list_of_tok(&token_head, line_of_commands[i]);
 	i++;
 	}
 	// after creating the list, I need to analyze each single node of the list?
-	
 	// a nice function that prints the list of tokens? 
 	print_list_tokens(&token_head);
 	return (0);
