f77859bb62434dcc63abb7800f7826ef3c831180
atucci <alessiotucci33@gmail.com>
2023-12-17 18:19:04 +0100

    working on the notes to understan

--- notes/examples/excevve.c
+++ notes/examples/excevve.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 15:59:54 by atucci            #+#    #+#             */
-/*   Updated: 2023/12/17 17:28:26 by atucci           ###   ########.fr       */
+/*   Updated: 2023/12/17 18:18:09 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 #include <unistd.h>
 #include <readline/readline.h>
 
-int main()
+int main(int ac, char *av[], char **env)
 {
+	(void) ac;
+	(void) av;
 	char	*input;
 
 	while (1)
 	{
 		input = readline("Enter command: ");
 		char *args[] = {input, NULL};
-		char *env_args[] = {"PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin", NULL};
 		if (fork() == 0)
 		{
-			execve(args[0], args, env_args);
+			execve("/bin/ls", args, env);
 			perror("execve");   // execve returns only on error
 			exit(EXIT_FAILURE);
 		}
