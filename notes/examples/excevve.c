ad688f7fbca41ebe020b90211a76b292c271ef3f
atucci <alessiotucci33@gmail.com>
2023-12-17 18:33:17 +0100

    starting to understand a little bit

--- notes/examples/excevve.c
+++ notes/examples/excevve.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 15:59:54 by atucci            #+#    #+#             */
-/*   Updated: 2023/12/17 18:18:09 by atucci           ###   ########.fr       */
+/*   Updated: 2023/12/17 18:32:29 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <readline/readline.h>
+#include <string.h>
 
 int main(int ac, char *av[], char **env)
 {
 	(void) ac;
 	(void) av;
 	char	*input;
+	char	*command;
 
 	while (1)
 	{
 		input = readline("Enter command: ");
-		char *args[] = {input, NULL};
+		command = malloc(strlen("/bin/") + strlen(input) + 1);  // allocate memory for the command
+		strcpy(command, "/bin/");  // copy "/bin/" to command
+		strcat(command, input);  // append input to command
+		char *args[] = {command, NULL};
 		if (fork() == 0)
 		{
-			execve("/bin/ls", args, env);
+			execve(command, args, env);
 			perror("execve");   // execve returns only on error
 			exit(EXIT_FAILURE);
 		}
 		else
 			wait(NULL);  // parent waits for the child to finish
 		free(input);
+		free(command);
 	}
 	return 0;
 }
