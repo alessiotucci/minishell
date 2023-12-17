dae69a4328bb784d4697ffc3f02b664d9ae1b50f
EnricoGiraldi <enricogiraldi@LAPTOP-VDCK318R.localdomain>
2023-12-17 22:23:11 +0100

    new builtin

--- builtin_enrico_2/minishell_pwd.c
+++ builtin_enrico_2/minishell_pwd.c
@@ -1,26 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    minishell_env.c                                   :+:      :+:    :+:   */
+/*   minishell_pwd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enricogiraldi <enricogiraldi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/05 17:26:42 by enricogiral       #+#    #+#             */
-/*   Updated: 2023/12/05 17:27:32 by enricogiral      ###   ########.fr       */
+/*   Created: 2023/12/11 18:23:44 by enricogiral       #+#    #+#             */
+/*   Updated: 2023/12/17 16:59:47 by enricogiral      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "builtin.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int minishell_env(t_env *env_list) 
-{
-    // Stampa tutte le variabili d'ambiente
-    t_env *current = env_list;
-    while (current != NULL) 
-    {
-        printf("%s=%s\n", current->key, current->value);
-        current = current->next;
+void minishell_pwd() {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s\n", cwd);
+    } else {
+        perror("getcwd");
     }
+}
 
-    return 0;  // Ritorna il codice di uscita appropriato
+int main() {
+    minishell_pwd();
+    return 0;
 }
