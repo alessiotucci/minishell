6e908ee58d0e74e6897212f9acc0eac32d807d2d
atucci <alessiotucci33@gmail.com>
2023-12-24 14:07:24 +0100

    the good work on execve

--- notes/EX_exceve_examle/excevve.c
+++ notes/EX_exceve_examle/excevve.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 15:59:54 by atucci            #+#    #+#             */
-/*   Updated: 2023/12/24 12:01:06 by atucci           ###   ########.fr       */
+/*   Updated: 2023/12/24 14:03:14 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,52 @@ int main(int ac, char *av[], char **env)
 	(void) av;
 	char	*input;
 	char	*command;
-
+	char	*path = NULL;
+	char	**directs;
+	// Find the PATH in the env array
+	for (int i = 0; env[i] != NULL; i++)
+	{
+		if (strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path = env[i] + 5; // Skip the "PATH=" part
+			break;
+		}
+	}
+	//printf("Path: (%s%s%s)\n", GREEN, path, RESET);
+	directs = ft_split(path, ':');
 	while (1)
 	{
 		input = readline(RED"miniHell "RESET);
-		command = malloc(strlen("/bin/") + strlen(input) + 1);  // allocate memory for the command
-		strcpy(command, "/bin/");// copy "/bin/" to command
-		strcat(command, input);// append input to command
-		//char *args[] = {command, NULL};
-		char **args = ft_split(command, ' ');
+		char **input_parts = ft_split(input, ' ');
+		command = NULL;
+		for (int i = 0; directs[i] != NULL; i++)
+		{
+			char *possible_command = malloc(strlen(directs[i]) + strlen(input_parts[0]) + 2); // +2 for the '/' and '\0'
+			strcpy(possible_command, directs[i]);
+			strcat(possible_command, "/");
+			strcat(possible_command, input_parts[0]);
+			//printf("input_parts[0]: %s\n directs[%d]: %s\n", input_parts[0], i, directs[i]);
+			if (access(possible_command, X_OK) == 0)// Check if the command is executable
+			{
+				command = possible_command;
+				break;
+			}
+			free(possible_command);
+		}
+		if (command == NULL)
+		{
+			printf("Command not found\n");
+			free(input);
+			free(input_parts);
+			continue;
+		}
+		//char **args = ft_split(command, ' ');
 		if (fork() == 0)
 		{
-			printf("%sINSIDE CHILD PROCESS\n%s", YELLOW, RESET);
+//			printf("%sINSIDE CHILD PROCESS\n%s", YELLOW, RESET);
 			// int execve(const char *path, char *const argv[], char *const envp[]);
-			printf("const char *path: %s\n", command);
-			//execve(command, args, env);
-			execve(args[0], args, env);
+//			printf("const char *path: %s\n", command);
+			execve(command, input_parts, env);
 			perror("execve");// execve returns only on error
 			exit(EXIT_FAILURE);
 		}
@@ -47,6 +77,7 @@ int main(int ac, char *av[], char **env)
 			wait(NULL);// parent waits for the child to finish
 		free(input);
 		free(command);
+		free(input_parts);
 	}
 	return 0;
 }
