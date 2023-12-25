e2149186583fda58995f604743f814f199d6686c
atucci <alessiotucci33@gmail.com>
2023-12-25 08:32:37 +0100

    adding the hidden files

--- executor_folder/executor.c
+++ executor_folder/executor.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:25:22 by atucci            #+#    #+#             */
-/*   Updated: 2023/12/24 18:06:40 by atucci           ###   ########.fr       */
+/*   Updated: 2023/12/25 08:31:50 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,13 @@ void	executor(t_list_of_tok **head, char **envp)
 	char	**directs;
 	char	*command = NULL;
 	char	*possible_command;
+	
 	t_list_of_tok *current = find_command_in_list(head);
 	if (current == NULL)
 	{
 		printf("there is no command in the string sadly\n");
 		return ;
 	}
-	(void)head;
 	i = 0;
 	directs = find_path_env(envp);
 	while (directs[i] != NULL)
@@ -67,7 +67,6 @@ void	executor(t_list_of_tok **head, char **envp)
 		ft_strlcpy(possible_command, directs[i], y);
 		ft_strlcat(possible_command, "/", y);
 		ft_strlcat(possible_command, current->command_as_string, y);
-		//printf("input_parts[0]: %s\n directs[%d]: %s\n", input_parts[0], i, directs[i]);
 		if (access(possible_command, X_OK) == 0)// Check if the command is executable
 		{
 			command = possible_command;
@@ -76,9 +75,7 @@ void	executor(t_list_of_tok **head, char **envp)
 		free(possible_command);
 		i++;
 	}
-	//printf("commad %s\n", command);
 	// I need to create a array of string to pass to the execve!
-	// char **argv_for_exceve(t_list_of_tok **head);
 	char **test = argv_for_exceve(head);
 /*                                                                           */
 		if (fork() == 0)
