7049926ff6b32ff98dcdf1eac1c24b67f615d755
atucci <alessiotucci33@gmail.com>
2023-12-25 09:17:09 +0100

    adding the file_nas

---
+++ executor_folder/exec_files.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_files.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/25 09:14:57 by atucci            #+#    #+#             */
+/*   Updated: 2023/12/25 09:16:34 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+/* Opening a file for single redirection > */
+// int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+/* Opening a file for double redirection >> */
+// int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+
