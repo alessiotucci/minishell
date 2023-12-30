32bc2bb48e057cf4fff11ec1f6ba4351db7f48f8
EnricoGiraldi <enricogiraldi@LAPTOP-VDCK318R.localdomain>
2023-12-30 10:25:07 +0100

    last change in builtin

--- builtin_enrico_2/minishell_env.c
+++ builtin_enrico_2/minishell_env.c
@@ -6,28 +6,36 @@
 /*   By: enricogiraldi <enricogiraldi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 22:05:47 by enricogiral       #+#    #+#             */
-/*   Updated: 2023/12/17 22:06:31 by enricogiral      ###   ########.fr       */
+/*   Updated: 2023/12/30 10:14:22 by enricogiral      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void minishell_env(char *args[]) {
+void    minishell_env(char *args[]) 
+{
     // If no arguments provided, print the current environment variables
-    if (args[1] == NULL) {
+    if (args[1] == NULL) 
+    {
         extern char **environ;
-        for (char **env = environ; *env != NULL; env++) {
+        char **env = environ;
+        while (*env != NULL) 
+        {
             printf("%s\n", *env);
+            env++;
         }
-    } else {
+    } 
+    else 
+    {
         fprintf(stderr, "minishell_env: Too many arguments\n");
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     minishell_env(argv);
-
     return 0;
 }
+
 
--- builtin_enrico_2/minishell_unset.c
+++ builtin_enrico_2/minishell_unset.c
@@ -6,7 +6,7 @@
 /*   By: enricogiraldi <enricogiraldi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 16:16:44 by enricogiral       #+#    #+#             */
-/*   Updated: 2023/12/29 16:17:19 by enricogiral      ###   ########.fr       */
+/*   Updated: 2023/12/30 10:24:13 by enricogiral      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,27 @@
 void minishell_unset(char *args[]) 
 {
     // Check if there are arguments to unset
-    if (args[1] == NULL) 
+    if (args[1] == NULL)
     {
-        fprintf(stderr, "minishell_unset: Missing argument\n");
+        printf("minishell_unset: Missing argument\n");
         return;
     }
 
-    // Iterate through the arguments and unset environment variables
-    for (int i = 1; args[i] != NULL; i++) 
+    // Unset the first argument
+    if (unsetenv(args[1]) != 0) 
     {
-        if (unsetenv(args[i]) != 0) {
-            perror("minishell_unset");
+        printf("minishell_unset: Error unsetting %s\n", args[1]);
+    }
+
+    // If there are more arguments, unset them one by one
+    int i = 2;
+    while (args[i] != NULL) 
+    {
+        if (unsetenv(args[i]) != 0) 
+        {
+            printf("minishell_unset: Error unsetting %s\n", args[i]);
         }
+        i++;
     }
 }
 
