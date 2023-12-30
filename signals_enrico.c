1bc2b2ee4e0bfbb3706f51775f45bfc5662e787f
EnricoGiraldi <enricogiraldi@LAPTOP-VDCK318R.localdomain>
2023-12-30 12:17:21 +0100

    signals enrico

--- signals_enrico.c
+++ signals_enrico.c
@@ -8,7 +8,8 @@
 int exit_code = 0;
 
 // Signal handler for SIGINT (Ctrl+C)
-void handle_ctrl_c(int sig) {
+void handle_ctrl_c(int sig) 
+{
     (void)sig;
     rl_replace_line("", 0);  // Clear the current line
     rl_on_new_line();        // Move the cursor to a new line
@@ -16,14 +17,16 @@ void handle_ctrl_c(int sig) {
 }
 
 // Signal handler for SIGQUIT (Ctrl+\)
-void handle_ctrl_backslash(int sig) {
+void handle_ctrl_backslash(int sig) 
+{
     (void)sig;
     printf("\nHandling signal with Ctrl+\\ \n");
     exit(exit_code);  // Adjust exit_code as needed
 }
 
 // Signal handler for SIGTERM (default termination signal)
-void handle_termination(int sig) {
+void handle_termination(int sig) 
+{
     (void)sig;
     printf("\nHandling termination signal \n");
     exit(exit_code);  // Adjust exit_code as needed
