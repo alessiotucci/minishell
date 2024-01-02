9f431c2dfd5631b29605ea7641dc6d05f11e821c
EnricoGiraldi <enricogiraldi@LAPTOP-VDCK318R.localdomain>
2024-01-02 18:17:51 +0100

    export.c

--- builtin_enrico_2/minishell_export.c
+++ builtin_enrico_2/minishell_export.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,29 +9,55 @@ void minishell_export(char *args[])
     if (args[1] == NULL) 
     {
         extern char **environ;
-        for (char **env = environ; *env != NULL; env++) 
+        char **env = environ;
+        while (*env != NULL) 
         {
-            printf("%s\n", *env);
+            write(STDOUT_FILENO, *env, strlen(*env));
+            write(STDOUT_FILENO, "\n", 1);
+            env++;
         }
-    } else {
-        // Iterate through the arguments and set environment variables
-        for (int i = 1; args[i] != NULL; i++) 
+    } 
+    else 
+    {
+        // Process arguments without using strtok
+        int i = 1;
+        while (args[i] != NULL) 
         {
-            char *key = strtok(args[i], "=");
-            char *value = strtok(NULL, "");
+            char *arg = args[i];
+            char *key = arg;
 
-            // Check if the format is key=value
-            if (key != NULL && value != NULL) 
+            // Find the position of '=' in the argument
+            char *equal_sign = strchr(arg, '=');
+
+            // If '=' is found and not at the beginning or end
+            if (equal_sign != NULL && equal_sign != arg && equal_sign[1] != '\0') 
             {
-                if (setenv(key, value, 1) != 0) 
+                *equal_sign = '\0';  // Null-terminate the key
+                char *value = equal_sign + 1;
+
+                // Check if the key and value are not empty
+                if (*key != '\0' && *value != '\0') 
                 {
-                    perror("minishell_export");
+                    if (setenv(key, value, 1) != 0) 
+                    {
+                        perror("minishell_export");
+                    }
+                } 
+                else 
+                {
+                    char error_message[100];
+                    sprintf(error_message, "minishell_export: Invalid format: %s\n", arg);
+                    write(STDERR_FILENO, error_message, strlen(error_message));
                 }
             } 
             else 
             {
-                fprintf(stderr, "minishell_export: Invalid format: %s\n", args[i]);
+                char error_message[100];
+                sprintf(error_message, "minishell_export: Invalid format: %s\n", arg);
+                write(STDERR_FILENO, error_message, strlen(error_message));
             }
+
+            i++;
         }
     }
 }
@@ -40,4 +67,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
