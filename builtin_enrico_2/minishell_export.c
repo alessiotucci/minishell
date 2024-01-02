67f36be0946697eb47e1c9431f13a6c41da09215
EnricoGiraldi <enricogiraldi@LAPTOP-VDCK318R.localdomain>
2024-01-02 18:10:18 +0100

    change in env

--- builtin_enrico_2/minishell_export.c
+++ builtin_enrico_2/minishell_export.c
@@ -2,25 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-void minishell_export(char *args[]) {
+void minishell_export(char *args[]) 
+{
     // If no arguments provided, print the current environment variables
-    if (args[1] == NULL) {
+    if (args[1] == NULL) 
+    {
         extern char **environ;
-        for (char **env = environ; *env != NULL; env++) {
+        for (char **env = environ; *env != NULL; env++) 
+        {
             printf("%s\n", *env);
         }
     } else {
         // Iterate through the arguments and set environment variables
-        for (int i = 1; args[i] != NULL; i++) {
+        for (int i = 1; args[i] != NULL; i++) 
+        {
             char *key = strtok(args[i], "=");
             char *value = strtok(NULL, "");
 
             // Check if the format is key=value
-            if (key != NULL && value != NULL) {
-                if (setenv(key, value, 1) != 0) {
+            if (key != NULL && value != NULL) 
+            {
+                if (setenv(key, value, 1) != 0) 
+                {
                     perror("minishell_export");
                 }
-            } else {
+            } 
+            else 
+            {
                 fprintf(stderr, "minishell_export: Invalid format: %s\n", args[i]);
             }
         }
