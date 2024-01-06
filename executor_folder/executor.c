5fa1a8f450f3e981ccbd2ca14dd2d2070248de08
atucci <alessiotucci33@gmail.com>
2024-01-06 14:19:24 +0100

    found some mistake

--- executor_folder/executor.c
+++ executor_folder/executor.c
@@ -6,11 +6,28 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:25:22 by atucci            #+#    #+#             */
-/*   Updated: 2024/01/06 13:44:59 by atucci           ###   ########.fr       */
+/*   Updated: 2024/01/06 14:18:48 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+const char *namey[] =
+{
+	"Command",
+	"Flag",
+	"Builtin",
+	"Parenthesis",
+	"Dollar",
+	"Pipes",
+	"Redirection in",
+	"Redirection out",
+	"Redirection appender",
+	"Here Documents",
+	"Logical Operator",
+	"Command Arguments",
+	"File Name "
+};
+
 
 /* 5 function in this file 
  * This function goes through the list and look for commands,
@@ -34,7 +51,7 @@ t_list_of_tok	*find_command_in_list(t_list_of_tok **head)
  * it has being copy and paste from bingAi again
  *
  * */
-static void	find_redirect(t_list_of_tok *cmd_node)
+static int	find_redirect(t_list_of_tok *cmd_node)
 {
 	t_list_of_tok	*iterator;
 
@@ -45,18 +62,19 @@ static void	find_redirect(t_list_of_tok *cmd_node)
 		{
 			if (iterator->next == NULL)
 			{
-				printf("zsh: parse error near `\\n\'");
-				return ;
+				printf("minishesh: parse error near '\\n\'\n");
+				return (1);
 			}
 			if (iterator->next->type == T_FILE_NAME)
 			{
 				cmd_node->file_name = ft_strdup(iterator->next->token);
 				//printf("cmd_node->token:(%s) and cmd_node->filename:(%s)\n", cmd_node->token, cmd_node->file_name);
-				return ;
+				return (0);
 			}
 		}
 		iterator = iterator->next;
 	}
+	return (0);
 }
 
 /*3)
@@ -65,6 +83,7 @@ static void	find_redirect(t_list_of_tok *cmd_node)
  */
 void	redirection_process(t_list_of_tok *cmd_nod, t_type_of_tok type)
 {
+	printf("FUNCTION-> redirection_process(); [%s] %s {%s}%s\n", cmd_nod->token, BG_RED, namey[cmd_nod->type], BG_RESET);
 	if (type == T_FLAG || type == T_COMMAND_ARGS)
 		redirection_process(cmd_nod->next, cmd_nod->next->type);
 	if (type == T_REDIR_IN)
@@ -106,15 +125,15 @@ void	*execute_command(char *command, char **args_a, char **envp, t_list_of_tok *
 	pid_t	fix_pid;
 	int	stdout_copy = dup(STDOUT_FILENO);
 
-	//printf("%sFunciton-> Execute_command()%s;\n\tCommand: {%s},\n\targs[1]: {%s}\n",BG_YELLOW, BG_RESET, command, args_a[1]);
-	//printf("\ncmd_nod->file_name != NULL (%s)\n", cmd_nod->file_name);
+	printf("%sFunciton-> Execute_command()%s;\n\tCommand: {%s},\n\targs[1]: {%s}\n",BG_YELLOW, BG_RESET, command, args_a[1]);
+	printf("\ncmd_nod->file_name != NULL (%s)\n", cmd_nod->file_name);
 	if (cmd_nod->file_name != NULL)
 		redirection_process(cmd_nod, cmd_nod->next->type); // here the fd are changed
 		piping_process(cmd_nod);
 	if (cmd_nod->type == T_BUILTIN)
 	{
-	//	fprintf(stdout, "Builtins:%s%s\t(%s)%s\n", BLUE, cmd_nod->token, command, RESET);
-	//	fprintf(stdout, "%s\tFd_in:%s %d %sFd_out:%s %d\n\n", RED, RESET,cmd_nod->fd_pipe_in, YELLOW, RESET, cmd_nod->fd_pipe_out);
+		fprintf(stdout, "Builtins: %s%s\t(%s)%s\n", BLUE, cmd_nod->token, command, RESET);
+		fprintf(stdout, "%s\tFd_in:%s %d %sFd_out:%s %d\n\n", RED, RESET,cmd_nod->fd_pipe_in, YELLOW, RESET, cmd_nod->fd_pipe_out);
 		which_built_in(cmd_nod, args_a, envp);
 		// Restore the original stdout file descriptor
 		dup2(stdout_copy, STDOUT_FILENO);
@@ -123,8 +142,8 @@ void	*execute_command(char *command, char **args_a, char **envp, t_list_of_tok *
 	}
 	else
 	{
-	//	fprintf(stdout, "Command:%s%s\t(%s)%s\n", GREEN, cmd_nod->token, command, RESET);
-	//	fprintf(stdout, "%s\tFd_in:%s %d %sFd_out:%s %d\n\n", RED, RESET,cmd_nod->fd_pipe_in, YELLOW, RESET, cmd_nod->fd_pipe_out);
+		fprintf(stdout, "Command: %s%s\t(%s)%s\n", GREEN, cmd_nod->token, command, RESET);
+		fprintf(stdout, "%s\tFd_in:%s %d %sFd_out:%s %d\n\n", RED, RESET,cmd_nod->fd_pipe_in, YELLOW, RESET, cmd_nod->fd_pipe_out);
 		fix_pid = fork();
 		if (fix_pid == 0)
 		{
@@ -159,14 +178,15 @@ int	executor(t_list_of_tok **head, char **envp)
 	cmd_node = find_command_in_list(head);
 	while (cmd_node != NULL)
 	{
-		find_redirect(cmd_node);
+		if (find_redirect(cmd_node) == 1)
+			break ;
 		find_pipes(cmd_node);
 		command = cmd_node->token;
 		if (cmd_node->type != T_BUILTIN)
 			command = find_path_command(cmd_node->token, envp);
 		if (command == NULL)
-			//return (free(command), printf(" Command not found: %s\n", cmd_node->token));
-			command = cmd_node->token;
+			return (free(command), printf(" Command not found: %s\n", cmd_node->token));
+			//command = cmd_node->token;
 		argoums = array_from_list(&cmd_node);
 		execute_command(command, argoums, envp, cmd_node);
 		cmd_node = find_command_in_list(&cmd_node->next);
