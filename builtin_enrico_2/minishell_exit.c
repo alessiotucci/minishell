c3c6a6c31841e0b033af76928da2d8df3db434d4
EnricoGiraldi <enricogiraldi@LAPTOP-VDCK318R.localdomain>
2024-01-08 08:20:13 +0100

    exit

--- builtin_enrico_2/minishell_exit.c
+++ builtin_enrico_2/minishell_exit.c
@@ -1,49 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell_exit.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: enricogiraldi <enricogiraldi@student.42    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 18:22:33 by enricogiral       #+#    #+#             */
-/*   Updated: 2023/12/17 22:19:10 by enricogiral      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>    // Inclusione per isdigit
+#include <string.h>   // Inclusione per strlen
 
-#include "minishell.h"
+static bool is_valid_exit_arg(char *args[]) {
+    int i, j;
 
-extern long long g_exit_status;
-
-static void exit_non_numeric_arg(void)
-{
-    g_exit_status = 2;
-    write(STDERR_FILENO, EXIT_NON_NUMERIC_ARG, ft_strlen(EXIT_NON_NUMERIC_ARG));
-    write(STDERR_FILENO, "\n", 1);
-}
-
-void cmd_exit(t_statement *s)
-{
-    write(STDOUT_FILENO, "exit\n", 5);
-    if (s->argc == 2)
-    {
-        if (is_all_digits_or_signals(s->argv[1]))
-        {
-            if (!fits_in_longlong(s->argv[1]))
-                exit_non_numeric_arg();
-            else
-                g_exit_status = ft_atoll(s->argv[1]);
+    i = 0;
+    while (args && args[i]) {
+        j = 0;
+        while (args[i][j]) {
+            if (args[i][j] == '-' || args[i][j] == '+')  // Aggiunto il controllo per i segni
+                j++;
+            if (!isdigit(args[i][j]))
+                return false;
+            j++;
         }
-        else
-            exit_non_numeric_arg();
+        i++;
     }
-    else if (s->argc > 2)
-    {
-        g_exit_status = EXIT_FAILURE;
-        write(STDERR_FILENO, EXIT_TOO_MANY_ARGS, ft_strlen(EXIT_TOO_MANY_ARGS));
-        write(STDERR_FILENO, "\n", 1);
-    }
-    else
-        g_exit_status = EXIT_SUCCESS;
-
-    exit(g_exit_status);
+    return true;
 }
+
+int main(int argc, char *argv[]) {
+    long exit_code;
+
+    if (argc > 1) {
+        if (!is_valid_exit_arg(argv + 1) || strlen(argv[1]) > 19) {
+            fprintf(stderr, "exit: not a valid argument\n");
+            exit_code = 255;
+        } else {
+            exit_code = atol(argv[1]);
+        }
+    } else {
+        exit_code = 0;  // Default exit code if no argument is provided
+    }
+
+    fprintf(stderr, "exit\n");
+    exit((int)exit_code);
+}
+
