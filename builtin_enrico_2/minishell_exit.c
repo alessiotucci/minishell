7d00e2ff1e43ca5f2884fb3dcf5d3cef69f8a182
Enrico Giraldi <enricogiraldi@MacBook-Pro-di-Enrico.local>
2024-01-09 17:30:36 +0100

    exit

--- builtin_enrico_2/minishell_exit.c
+++ builtin_enrico_2/minishell_exit.c
@@ -6,20 +6,16 @@
 #include <sys/wait.h>
 
 void cleanupResources() {
-    // Aggiungi qui la tua logica di pulizia delle risorse
-    // Ad esempio, chiudi file, dealloca memoria, ecc.
     printf("Pulizia delle risorse...\n");
 }
 
 void handleSignal(int signal) {
-    // Gestisci i segnali, se necessario
     printf("Ricevuto il segnale %d\n", signal);
     cleanupResources();
     exit(1);
 }
 
 void minishellExit(int exitCode) {
-    // Pulizia delle risorse prima dell'uscita
     cleanupResources();
 
     // Termina i processi figli
@@ -28,11 +24,8 @@ void minishellExit(int exitCode) {
         printf("Processo figlio %d terminato\n", childPid);
     }
 
-    // Altre azioni personalizzate, se necessario
     printf("Uscita con codice %d\n", exitCode);
 
     // Uscita dal programma
     exit(exitCode);
 }
-
-
