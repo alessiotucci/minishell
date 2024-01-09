21e000785fbec2ffe2a80dc652117c1d0bef99c5
atucci <alessiotucci33@gmail.com>
2024-01-09 13:36:38 +0100

    adding the small changes

--- executor_folder/pipex.c
+++ executor_folder/pipex.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 14:29:34 by atucci            #+#    #+#             */
-/*   Updated: 2024/01/08 10:45:25 by atucci           ###   ########.fr       */
+/*   Updated: 2024/01/09 12:40:10 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,14 @@ void	set_pipes(t_list_of_tok *first_cmd, t_list_of_tok *secnd_cmd)
 {
 	int	fd[2];// File descriptors for the pipe //
 
-	printf("***\nPiping [%s\t%s\t%s] with [%s\t%s\t%s]\n***\n",BG_GREEN, first_cmd->token,BG_RESET, BG_RED, secnd_cmd->token, BG_RESET);
+	printf("\nPiping [%s\t%s\t%s] with [%s\t%s\t%s]\n",BG_GREEN, first_cmd->token,BG_RESET, BG_RED, secnd_cmd->token, BG_RESET);
 	if (pipe(fd) == -1)
 		return (perror("pipe"));
 	// If pipe creation fails, print an error message and return //
 	first_cmd->out_file = fd[1];
 	secnd_cmd->in_file = fd[0];
-	// Set the write end of the pipe (fd[1]) as the output for the first command //
-	// Set the read end of the pipe (fd[0]) as the input for the second command //
-	
+	printf("Write end of the pipe (fd[1]) = %d as the output for %s the first: %s%s \n", fd[1],BG_GREEN, first_cmd->token, BG_RESET);
+	printf("Read end of the pipe (fd[0]) = %d as the input for  %s the second: %s%s \n", fd[0], BG_RED, secnd_cmd->token, BG_RESET);
 }
 /*
 void	set_pipes(t_list_of_tok *first_cmd, t_list_of_tok *secnd_cmd)
@@ -56,6 +55,9 @@ void	find_pipes(t_list_of_tok *head)
 		{
 			second_cmd = find_command_in_list(&(current->next));
 			set_pipes(first_cmd, second_cmd);
+			// closing the fd here ? 
+			close(first_cmd->out_file);
+			close(second_cmd->in_file);
 			break ;
 		}
 		current = current->next;
