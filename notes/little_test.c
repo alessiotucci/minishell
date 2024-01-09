7f7e18a22c0df08c2d532b819d13dc749b350f3c
Enrico Giraldi <enricogiraldi@MacBook-Pro-di-Enrico.local>
2024-01-09 17:28:35 +0100

    exit

--- notes/little_test.c
+++ notes/little_test.c
@@ -6,7 +6,7 @@
 /*   By: enricogiraldi <enricogiraldi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 13:03:32 by atucci            #+#    #+#             */
-/*   Updated: 2024/01/09 17:04:28 by enricogiral      ###   ########.fr       */
+/*   Updated: 2024/01/09 17:27:50 by enricogiral      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,37 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void cleanupResources() {
+    printf("Pulizia delle risorse...\n");
+}
+
+void handleSignal(int signal) {
+    printf("Ricevuto il segnale %d\n", signal);
+    cleanupResources();
+    exit(1);
+}
+
+void minishellExit(int exitCode) {
+    cleanupResources();
+
+    // Termina i processi figli
+    pid_t childPid;
+    while ((childPid = waitpid(-1, NULL, WNOHANG)) > 0) {
+        printf("Processo figlio %d terminato\n", childPid);
+    }
+
+    printf("Uscita con codice %d\n", exitCode);
+
+    // Uscita dal programma
+    exit(exitCode);
+}
+
+
+
 
 void minishellExit(int exitCode);
 
