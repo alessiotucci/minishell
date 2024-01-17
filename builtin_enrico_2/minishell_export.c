951d87c6070ba6df7709509d4c27947af822a0ca
Enrico Giraldi <enricogiraldi@MacBook-Pro-di-Enrico.local>
2024-01-17 11:39:47 +0100

    minishell_export

--- builtin_enrico_2/minishell_export.c
+++ builtin_enrico_2/minishell_export.c
@@ -1,50 +1,121 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell_export_2.c                               :+:      :+:    :+:   */
+/*   minishell_export.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enricogiraldi <enricogiraldi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 18:49:23 by enricogiral       #+#    #+#             */
-/*   Updated: 2024/01/06 18:51:50 by enricogiral      ###   ########.fr       */
+/*   Updated: 2024/01/17 11:38:21 by enricogiral      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+#define MAX_ENV_VARIABLES 100
+
+void print_export_format(char *var, char *value) {
+    printf("declare -x %s=\"%s\"\n", var, value);
+}
+
+void my_export(char *args[], char *env[]) 
 {
-    if (argc != 3) 
-    {
-        printf("Usage: %s VARIABLE VALUE\n", argv[0]);
-        return 1;
-    }
-
-    char *variable = argv[1];
-    char *value = argv[2];
-
-    // Set the environment variable
-    if (setenv(variable, value, 1) != 0) 
-    {
-        printf("Error setting environment variable\n");
-        perror(NULL); // This will print the error message to stderr
-        return 1;
-    }
-
-    // Print the exported variable and its value
-    printf("export %s=\"%s\"\n", variable, value);
-
-    // Example of using getenv to retrieve the value
-    char *retrievedValue = getenv(variable);
-    if (retrievedValue != NULL) 
-    {
-        printf("Retrieved value using getenv: %s\n", retrievedValue);
+    if (args[1] == NULL) {
+        // Visualizza l'elenco delle variabili d'ambiente
+        int i = 0;
+        while (env[i] != NULL) 
+        {
+            char *equal_sign = strchr(env[i], '=');
+            if (equal_sign != NULL) 
+            {
+                // Contiene un valore, visualizza nel formato richiesto
+                *equal_sign = '\0';  // Termina la stringa prima dell'uguale
+                char *value = equal_sign + 1;
+                print_export_format(env[i], value);
+            }
+            i++;
+        }
     } 
     else 
     {
-        printf("Environment variable not found\n");
-    }
+        // Analizza e imposta/aggiorna le variabili d'ambiente
+        int i = 1;
+        while (args[i] != NULL) 
+        {
+            char *equal_sign = strchr(args[i], '=');
+            if (equal_sign != NULL) 
+            {
+                // Contiene un valore, imposta/aggiorna manualmente
+                *equal_sign = '\0';  // Termina la stringa prima dell'uguale
+                char *value = equal_sign + 1;
+                char *existing_value = getenv(args[i]);
 
+                if (existing_value != NULL) 
+                {
+                    // Variabile già esistente, cerca e aggiorna manualmente
+                    int j = 0;
+                    while (env[j] != NULL) 
+                    {
+                        if (strncmp(env[j], args[i], strlen(args[i])) == 0) 
+                        {
+                            free(env[j]);  // Libera la memoria precedente
+                            env[j] = malloc(strlen(args[i]) + strlen(value) + 2);
+                            strcpy(env[j], args[i]);
+                            strcat(env[j], "=");
+                            strcat(env[j], value);
+                            print_export_format(args[i], value);
+                            break;
+                        }
+                        j++;
+                    }
+                } 
+                else 
+                {
+                    // Variabile non esistente, cerca spazio libero e imposta manualmente
+                    int j = 0;
+                    while (env[j] != NULL) 
+                    {
+                        j++;
+                    }
+                    env[j] = malloc(strlen(args[i]) + strlen(value) + 2);
+                    strcpy(env[j], args[i]);
+                    strcat(env[j], "=");
+                    strcat(env[j], value);
+                    print_export_format(args[i], value);
+                }
+            } 
+            else 
+            {
+                // Non contiene un valore, gestisci come se avesse un valore vuoto
+                char *existing_value = getenv(args[i]);
+
+                int j = 0;
+                while (env[j] != NULL) {
+                    if (existing_value == NULL) 
+                    {
+                        // Variabile non esistente, cerca spazio libero e imposta manualmente
+                        int k = 0;
+                        while (env[k] != NULL) 
+                        {
+                            k++;
+                        }
+                        env[k] = malloc(strlen(args[i]) + 1);
+                        strcpy(env[k], args[i]);
+                        print_export_format(args[i], "");
+                        break;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+    }
+}
+
+
+int main(int argc, char *argv[], char *env[]) {
+    my_export(argv, env);
     return 0;
 }
